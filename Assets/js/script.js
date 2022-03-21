70e219580d88d41a638b4219cc50b404b339c927
Andre <profandresjara@gmail.com>
2022-03-21 18:13:31 -0300

    Commit 1

---
+++ Assets/js/script.js
@@ -0,0 +1,17 @@
+$(document) .ready(function() {
+ 
+    $("a") .click(function() {
+        var gato = this.hash
+ 
+    $("html, body").animate (
+        {
+            scrollTop: $(gato).offset().top
+        },
+        3000
+    )
+    })
+})
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
